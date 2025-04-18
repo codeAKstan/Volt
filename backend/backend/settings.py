881c724b0af7fe7c13f0cc760ebf7d36d63821a9
codeAKstan <boystanismoses@gmail.com>
2025-04-18 20:47:56 +0100

    Modified setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -194,7 +194,7 @@ SIMPLE_JWT = {
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['https://volt-lyart.vercel.app/']
+CORS_ALLOWED_ORIGINS = ['https://volt-coral.vercel.app/']
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE',
